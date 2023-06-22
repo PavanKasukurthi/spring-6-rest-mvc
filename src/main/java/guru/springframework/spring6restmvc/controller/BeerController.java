1b4fd982a0b450c953862d69001c461183b6f75a
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-06-22 23:54:42 +0530

    validations

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -2,12 +2,14 @@ package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.service.BeerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +18,13 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/beer")
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v1/beer";
     public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
     @Autowired
     private final BeerService beerService;
-    @PatchMapping("{beerId}")
+    @PatchMapping(BEER_PATH_ID)
     public ResponseEntity updateBeerPatchById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDTO beer){
 
         beerService.patchBeerById(beerId, beer);
@@ -31,7 +32,7 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/{beerId}")
+    @DeleteMapping(BEER_PATH_ID)
     public ResponseEntity deleteById(@PathVariable("beerId") UUID beerId){
 
        if(! beerService.deleteById(beerId)){
@@ -40,8 +41,8 @@ public class BeerController {
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
-    @PutMapping("{beerId}")
-    public ResponseEntity updateById(@PathVariable("beerId") UUID beerId,@RequestBody BeerDTO beer){
+    @PutMapping(BEER_PATH_ID)
+    public ResponseEntity updateById( @PathVariable("beerId") UUID beerId,@Validated @RequestBody BeerDTO beer){
 
         if(beerService.updateBeerById(beerId, beer).isEmpty()){
             throw new NotFoundException();
@@ -50,24 +51,24 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity handlePost(@RequestBody BeerDTO beer){
+    @PostMapping(BEER_PATH)
+    public ResponseEntity handlePost(@Validated @RequestBody BeerDTO beer){
 
-        BeerDTO savedBeer =beerService.saveNewBeer(beer);
+        BeerDTO savedBeer = beerService.saveNewBeer(beer);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString() );
+        headers.add("Location", BEER_PATH + "/" + savedBeer.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(value = BEER_PATH)
     public List<BeerDTO> listBeers(){
         return beerService.listBeers();
     }
 
-    @RequestMapping(value = "/{beerId}", method = RequestMethod.GET)
+    @GetMapping(value = BEER_PATH_ID)
     public BeerDTO getBeerById(@PathVariable("beerId") UUID id){
 
         log.debug("Get beer by id in controller");
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,14 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,13 +18,36 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void testSaveBeerNameTooLong(){
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer savedBeer = beerRepository.save(Beer.builder()
+                    .beerName("My Beer sskjandnxcksjdsskjandnxcksjdsskjandnxcksjdsskjandnxcksjdsskjandnxcksjdsskjandnxcksjdsskjandnxcksjdsskjandnxcksjd")
+                    .beerStyle(BeerStyle.GOSE)
+                    .upc("123456")
+                    .price(new BigDecimal("19.99"))
+                    .build());
+
+            beerRepository.flush();
+//        assertThat(savedBeer).isNotNull();
+//        assertThat(savedBeer.getId()).isNotNull();
+        });
+
+    }
+
+    @Test
     void testSaveBeer(){
         Beer savedBeer = beerRepository.save(Beer.builder()
                         .beerName("My Beer")
+                        .beerStyle(BeerStyle.GOSE)
+                        .upc("123456")
+                        .price(new BigDecimal("19.99"))
                 .build());
 
-                assertThat(savedBeer).isNotNull();
-                assertThat(savedBeer.getId()).isNotNull();
+        beerRepository.flush();
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getId()).isNotNull();
     }
 
 }
